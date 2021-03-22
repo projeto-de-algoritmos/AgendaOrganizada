148f6323c5ea8e25c9716fea115eac7df47109fb
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2021-03-22 11:14:24 -0300

    feat: save schedule on localStorage and limit inputs values

---
+++ agendaorganizada/src/utils/getTodayDate.ts
@@ -0,0 +1,14 @@
+export default function getTodayDate() {
+  const today = new Date();
+  let dd = today.getDate().toString();
+  let mm = (today.getMonth() + 1).toString(); // January is 0!
+  const yyyy = today.getFullYear().toString();
+  if (Number(dd) < 10) {
+    dd = `0${dd}`;
+  }
+  if (Number(mm) < 10) {
+    mm = `0${mm}`;
+  }
+  console.log(`${yyyy}-${mm}-${dd}`);
+  return `${yyyy}-${mm}-${dd}`;
+}
