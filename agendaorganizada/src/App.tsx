4f5621b50f94404ddd71b5935a1a5ab1c4152221
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2021-03-22 11:14:24 -0300

    feat: save schedule on localStorage and limit inputs values

--- agendaorganizada/src/App.tsx
+++ agendaorganizada/src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ITask from './utils/itask';
 import minimizeLateness from './utils/minimizeLateness';
 import transformDate from './utils/transformDate';
+import getTodayDate from './utils/getTodayDate';
 import './index.css';
 
 // Quais são os dados da agenda?
@@ -19,12 +20,16 @@ const App: React.FC = () => {
   const [tasks, setTasks] = useState<Array<ITask>>([]);
   const [lateness, setLateness] = useState<number>(0);
 
-  // const printaDados = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-  //   e.preventDefault();
-  //   console.log(name);
-  //   console.log(duration);
-  //   console.log(deadline);
-  // };
+  useEffect(() => {
+    const tasksArray: ITask[] = JSON.parse(localStorage.getItem('tasksArray') || '[]');
+    setLateness(minimizeLateness(tasksArray));
+    setTasks(tasksArray);
+  }, []);
+
+  const deleteSchedule = () => {
+    localStorage.removeItem('tasksArray');
+    setTasks([]);
+  };
 
   const submitTask = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -42,6 +47,8 @@ const App: React.FC = () => {
     setName('');
     setDuration(0);
     setDeadline('');
+
+    localStorage.setItem('tasksArray', JSON.stringify(tasksArray));
   };
 
   return (
@@ -56,6 +63,7 @@ const App: React.FC = () => {
             value={name}
             placeholder="Nome da tarefa"
             onChange={(e) => setName(e.target.value)}
+            required
           />
         </label>
         <label htmlFor="duration">
@@ -66,6 +74,8 @@ const App: React.FC = () => {
             id="duration"
             value={duration}
             onChange={(e) => setDuration(Number(e.target.value))}
+            required
+            min="0"
           />
         </label>
         <label htmlFor="deadline">
@@ -76,6 +86,8 @@ const App: React.FC = () => {
             id="deadline"
             value={deadline}
             onChange={(e) => setDeadline(e.target.value)}
+            required
+            min={getTodayDate()}
           />
         </label>
         <label htmlFor="deadlineHour">
@@ -86,17 +98,21 @@ const App: React.FC = () => {
             id="deadline_hour"
             value={deadlineHour}
             onChange={(e) => setDeadlineHour(e.target.value)}
+            required
           />
         </label>
         <button type="submit">Cadastrar</button>
       </form>
       <div className="taskscontainer">
-        <h1>
-          Atraso mínimo para essa agenda:
-          {' '}
-          {lateness}
-          h
-        </h1>
+        <div className="tasksheader">
+          <h1>
+            Atraso mínimo:
+            {' '}
+            {lateness}
+            h
+          </h1>
+          <button onClick={deleteSchedule} type="button">Limpar agenda</button>
+        </div>
         <div className="tasks">
           <div className="task">
             <p><strong>Tarefa:</strong></p>
